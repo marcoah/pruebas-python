cf613635a58c75d194ce14955493e97fdd4ce221
Geologia y Mapas <geologiaymapas@gmail.com>
2022-10-01 23:31:18 -0400

    Trabajo con la libreria panda

--- pandas-example.py
+++ pandas-example.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('data.csv')
 
-print(df.to_string()) 
+#print(df.to_string()) 
+
+print(df.info())  #informacion de la data
+#print(df.tail()) 
+#print(df.head(10)) #imprime primeros 10
